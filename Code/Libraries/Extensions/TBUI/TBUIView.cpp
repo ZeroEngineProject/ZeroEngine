1d3378d87fc428bc91cb00416688a7130fd2076c
Robert Campbell <waprave@gmail.com>
2024-03-23 23:47:16 -0400

    WIP

--- Code/Libraries/Extensions/TBUI/TBUIView.cpp
+++ Code/Libraries/Extensions/TBUI/TBUIView.cpp
@@ -7,4 +7,21 @@ ZilchDefineType(TBUIView, builder, type)
 {
   ZeroBindDocumented();
 }
+TBUIView::TBUIView()
+{
+  mUI = TBUI::GetInstance();
+  mRenderer = mUI->GetRenderer();
+
+  // Set gravity all so we resize correctly
+  SetGravity(tb::WIDGET_GRAVITY_ALL);
+
+  // Set initial size for view
+  tb::TBRect rect = mUI->GetRootWidget()->GetRect();
+  SetSize(rect.w, rect.h);
+
+  mUI->AddView(this);
+}
+void TBUIView::UpdateBatches()
+{
+}
 } // namespace Zero
--- Code/Libraries/Extensions/TBUI/TBUIView.hpp
+++ Code/Libraries/Extensions/TBUI/TBUIView.hpp
@@ -8,5 +8,15 @@ class TBUIView : public tb::TBWidget
 public:
   /// Meta Initialization.
   ZilchDeclareType(TBUIView, TypeCopyMode::ReferenceType);
+
+  TBUIView();
+
+private:
+  void UpdateBatches();
+
+private:
+  TBUI* mUI = nullptr;
+  TBUIRenderer* mRenderer = nullptr;
+  Array<TBUIBatch> mBatches;
 };
 } // namespace Zero
