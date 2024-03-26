586951c1660b43aec908d046edfba3bcf1e713cd
Robert Campbell <waprave@gmail.com>
2024-03-26 02:40:35 -0400

    Added UIView concept

--- Code/Libraries/Extensions/TBUI/TBUI.cpp
+++ Code/Libraries/Extensions/TBUI/TBUI.cpp
@@ -32,44 +32,46 @@ TBUI::~TBUI()
 
 void TBUI::OnUiUpdate(UpdateEvent* event)
 {
-  OsShell* shell = Z::gEngine->has(OsShell);
-  if (shell != nullptr)
-  {
-    OsWindow* osWindow = shell->GetWindow(0);
-    IntVec2 clientSize = osWindow->GetClientSize();
-    IntVec2 clientPos = osWindow->GetMonitorClientPosition();
+  IntVec2 clientSize = mOsWindow->GetClientSize();
+  IntVec2 clientPos = mOsWindow->GetMonitorClientPosition();
 
-    mRoot->SetRect(tb::TBRect(clientPos.x, clientPos.y, clientSize.x, clientSize.y));
-    tb::TBAnimationManager::Update();
-    mRoot->InvokeProcessStates();
-    mRoot->InvokeProcess();
+  mRoot->SetRect(tb::TBRect(clientPos.x, clientPos.y, clientSize.x, clientSize.y));
+  tb::TBAnimationManager::Update();
+  mRoot->InvokeProcessStates();
+  mRoot->InvokeProcess();
 
-    tb::TBMessageHandler::ProcessMessages();
-  }
+  tb::TBMessageHandler::ProcessMessages();
 }
 
 void TBUI::OnUiRenderUpdate(Event* event)
 {
-  OsShell* shell = Z::gEngine->has(OsShell);
-  OsWindow* osWindow = shell->GetWindow(0);
-  IntVec2 clientSize = osWindow->GetClientSize();
-  //IntVec2 clientPos = osWindow->GetMonitorClientPosition();
-  if (shell != nullptr)
+  forRange (TBUIView* view, mViews.All())
   {
-    mRenderer->BeginPaint(clientSize.x, clientSize.y);
-    tb::TBWidget::PaintProps props;
-    mRoot->InvokePaint(props);
-    //mRoot->GetFont()->DrawString(5, 5, tb::TBColor(255, 255, 255), "Hello from TB");
-    mRenderer->EndPaint();
+    view->UpdateBatches();
+  }
+
+  forRange (TBUIView* view, mViews.All())
+  {
+    view->RenderBatches();
   }
 }
 
 void TBUI::OnEngineUpdate(UpdateEvent* event)
 {
+  if (mHookEvents)
+  {
+    ConnectThisTo(Z::gEngine->has(TimeSystem), "UiUpdate", OnUiUpdate);
+    ConnectThisTo(Z::gEngine->has(GraphicsEngine), "UiRenderUpdate", OnUiRenderUpdate);
+
+    mHookEvents = false;
+  }
 }
 
 void TBUI::OnInitialize(Event* event)
 {
+  OsShell* shell = Z::gEngine->has(OsShell);
+  mOsWindow = shell->GetWindow(0);
+
   if (!tb::tb_core_is_initialized())
   {
     if (tb::tb_core_init(mRenderer))
@@ -122,90 +124,43 @@ void TBUI::OnInitialize(Event* event)
         font->RenderGlyphs(" !\"#$%&'()*+,-./"
                            "0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~•·åäöÅÄÖ");
 
-      if (mRoot == nullptr)
-      {
-        OsShell* shell = Z::gEngine->has(OsShell);
-        OsWindow* osWindow = shell->GetWindow(0);
-        IntVec2 clientSize = osWindow->GetClientSize();
-        IntVec2 clientPos = osWindow->GetMonitorClientPosition();
+      IntVec2 clientSize = mOsWindow->GetClientSize();
+      IntVec2 clientPos = mOsWindow->GetMonitorClientPosition();
 
-        mRoot = new tb::TBWidget();
-        mRoot->SetRect(tb::TBRect(clientPos.x, clientPos.y, clientSize.x, clientSize.y));
-        // mRoot->SetOpacity(0.1);
-
-        tb::TBWindow* mainWindow = new tb::TBWindow();
-
-        mRoot->AddChild(mainWindow);
-
-        tb::TBNode node;
-        if (node.ReadFile("@TBUI/demo/ui_resources/test_ui.tb.txt"))
-        {
-          tb::g_widgets_reader->LoadNodeTree(mainWindow, &node);
-
-          // Get title from the WindowInfo section (or use "" if not specified)
-          mainWindow->SetText(node.GetValueString("WindowInfo>title", ""));
-
-          const tb::TBRect parent_rect(
-              0, 0, mainWindow->GetParent()->GetRect().w, mainWindow->GetParent()->GetRect().h);
-          const tb::TBDimensionConverter* dc = tb::g_tb_skin->GetDimensionConverter();
-          tb::TBRect window_rect = mainWindow->GetResizeToFitContentRect();
-
-          // Use specified size or adapt to the preferred content size.
-          tb::TBNode* tmp = node.GetNode("WindowInfo>size");
-          if (tmp && tmp->GetValue().GetArrayLength() == 2)
-          {
-            window_rect.w = dc->GetPxFromString(tmp->GetValue().GetArray()->GetValue(0)->GetString(), window_rect.w);
-            window_rect.h = dc->GetPxFromString(tmp->GetValue().GetArray()->GetValue(1)->GetString(), window_rect.h);
-          }
-
-          // Use the specified position or center in parent.
-          tmp = node.GetNode("WindowInfo>position");
-          if (tmp && tmp->GetValue().GetArrayLength() == 2)
-          {
-            window_rect.x = dc->GetPxFromString(tmp->GetValue().GetArray()->GetValue(0)->GetString(), window_rect.x);
-            window_rect.y = dc->GetPxFromString(tmp->GetValue().GetArray()->GetValue(1)->GetString(), window_rect.y);
-          }
-          else
-            window_rect = window_rect.CenterIn(parent_rect);
-
-          // Make sure the window is inside the parent, and not larger.
-          window_rect = window_rect.MoveIn(parent_rect).Clip(parent_rect);
-
-          mainWindow->SetRect(window_rect);
-
-          // mainWindow->SetOpacity(1);
-        }
-
-        // ConnectThisTo(osWindow, Events::OsResized, OnOsResize);
-        ConnectThisTo(osWindow, Events::OsMouseDown, OnOsMouseDown);
-        ConnectThisTo(osWindow, Events::OsMouseUp, OnOsMouseUp);
-        ConnectThisTo(osWindow, Events::OsMouseMove, OnOsMouseMoved);
-
-        // ConnectThisTo(osWindow, Events::OsWindowBorderHitTest, OnOsWindowBorderHitTest);
-
-        // ConnectThisTo(osWindow, Events::OsMouseScroll, OnOsMouseScroll);
-
-        // ConnectThisTo(osWindow, Events::OsKeyTyped, OnOsKeyTyped);
-        // ConnectThisTo(osWindow, Events::OsKeyRepeated, OnOsKeyDown);
-        // ConnectThisTo(osWindow, Events::OsKeyDown, OnOsKeyDown);
-        // ConnectThisTo(osWindow, Events::OsKeyUp, OnOsKeyUp);
-
-        // ConnectThisTo(osWindow, Events::OsFocusGained, OnOsFocusGained);
-        // ConnectThisTo(osWindow, Events::OsFocusLost, OnOsFocusLost);
-
-        // ConnectThisTo(osWindow, Events::OsMouseFileDrop, OnOsMouseDrop);
-        // ConnectThisTo(osWindow, Events::OsPaint, OnOsPaint);
-
-        // ConnectThisTo(osWindow, Events::OsClose, OnClose);
-      }
+      mRoot = new tb::TBWidget();
+      mRoot->SetRect(tb::TBRect(clientPos.x, clientPos.y, clientSize.x, clientSize.y));
+      // mRoot->SetOpacity(0.1);
+      SetupDemo();
 
       // Give the root widget a background skin
       mRoot->SetSkinBg(TBIDC("background"));
 
       tb::TBWidgetsAnimationManager::Init();
 
-      ConnectThisTo(Z::gEngine->has(TimeSystem), "UiUpdate", OnUiUpdate);
-      ConnectThisTo(Z::gEngine->has(GraphicsEngine), "UiRenderUpdate", OnUiRenderUpdate);
+      // ConnectThisTo(osWindow, Events::OsResized, OnOsResize);
+      ConnectThisTo(mOsWindow, Events::OsMouseDown, OnOsMouseDown);
+      ConnectThisTo(mOsWindow, Events::OsMouseUp, OnOsMouseUp);
+      ConnectThisTo(mOsWindow, Events::OsMouseMove, OnOsMouseMoved);
+
+      // ConnectThisTo(mOsWindow, Events::OsWindowBorderHitTest, OnOsWindowBorderHitTest);
+
+      // ConnectThisTo(mOsWindow, Events::OsMouseScroll, OnOsMouseScroll);
+
+      // ConnectThisTo(mOsWindow, Events::OsKeyTyped, OnOsKeyTyped);
+      // ConnectThisTo(mOsWindow, Events::OsKeyRepeated, OnOsKeyDown);
+      // ConnectThisTo(mOsWindow, Events::OsKeyDown, OnOsKeyDown);
+      // ConnectThisTo(mOsWindow, Events::OsKeyUp, OnOsKeyUp);
+
+      // ConnectThisTo(mOsWindow, Events::OsFocusGained, OnOsFocusGained);
+      // ConnectThisTo(mOsWindow, Events::OsFocusLost, OnOsFocusLost);
+
+      // ConnectThisTo(mOsWindow, Events::OsMouseFileDrop, OnOsMouseDrop);
+      // ConnectThisTo(mOsWindow, Events::OsPaint, OnOsPaint);
+
+      // ConnectThisTo(mOsWindow, Events::OsClose, OnClose);
+
+      // ConnectThisTo(Z::gEngine->has(TimeSystem), "UiUpdate", OnUiUpdate);
+      // ConnectThisTo(Z::gEngine->has(GraphicsEngine), "UiRenderUpdate", OnUiRenderUpdate);
     }
   }
 }
@@ -237,16 +192,24 @@ tb::MODIFIER_KEYS GetModifierKeys(OsMouseEvent* mouseEvent)
 
 void TBUI::OnOsMouseUp(OsMouseEvent* mouseEvent)
 {
+  if (mouseEvent->mTerminated)
+    return;
+
   tb::MODIFIER_KEYS modifierKeys = GetModifierKeys(mouseEvent);
 
-  mRoot->InvokePointerUp(mouseEvent->ClientPosition.x, mouseEvent->ClientPosition.y, modifierKeys, false);
+  if (mRoot->InvokePointerUp(mouseEvent->ClientPosition.x, mouseEvent->ClientPosition.y, modifierKeys, false))
+    mouseEvent->Terminate();
 }
 
 void TBUI::OnOsMouseDown(OsMouseEvent* mouseEvent)
 {
+  if (mouseEvent->mTerminated)
+    return;
+
   tb::MODIFIER_KEYS modifierKeys = GetModifierKeys(mouseEvent);
 
-  mRoot->InvokePointerDown(mouseEvent->ClientPosition.x, mouseEvent->ClientPosition.y, 1, modifierKeys, false);
+  if (mRoot->InvokePointerDown(mouseEvent->ClientPosition.x, mouseEvent->ClientPosition.y, 1, modifierKeys, false))
+    mouseEvent->Terminate();
 }
 
 void TBUI::OnOsMouseMoved(OsMouseEvent* mouseEvent)
@@ -254,6 +217,58 @@ void TBUI::OnOsMouseMoved(OsMouseEvent* mouseEvent)
   tb::MODIFIER_KEYS modifierKeys = GetModifierKeys(mouseEvent);
 
   mRoot->InvokePointerMove(mouseEvent->ClientPosition.x, mouseEvent->ClientPosition.y, modifierKeys, false);
+}
+
+void TBUI::SetupDemo()
+{
+  if (mDemoSetup)
+    return;
+
+  TBUIView* view = new TBUIView();
+  mMainWindow = new tb::TBWindow();
+
+  view->AddChild(mMainWindow);
+
+  tb::TBNode node;
+  if (node.ReadFile("@TBUI/demo/ui_resources/test_ui.tb.txt"))
+  {
+    tb::g_widgets_reader->LoadNodeTree(mMainWindow, &node);
+
+    // Get title from the WindowInfo section (or use "" if not specified)
+    mMainWindow->SetText(node.GetValueString("WindowInfo>title", ""));
+
+    const tb::TBRect parent_rect(0, 0, mMainWindow->GetParent()->GetRect().w, mMainWindow->GetParent()->GetRect().h);
+    const tb::TBDimensionConverter* dc = tb::g_tb_skin->GetDimensionConverter();
+    tb::TBRect window_rect = mMainWindow->GetResizeToFitContentRect();
+
+    // Use specified size or adapt to the preferred content size.
+    tb::TBNode* tmp = node.GetNode("WindowInfo>size");
+    if (tmp && tmp->GetValue().GetArrayLength() == 2)
+    {
+      window_rect.w = dc->GetPxFromString(tmp->GetValue().GetArray()->GetValue(0)->GetString(), window_rect.w);
+      window_rect.h = dc->GetPxFromString(tmp->GetValue().GetArray()->GetValue(1)->GetString(), window_rect.h);
+    }
+
+    // Use the specified position or center in parent.
+    tmp = node.GetNode("WindowInfo>position");
+    if (tmp && tmp->GetValue().GetArrayLength() == 2)
+    {
+      window_rect.x = dc->GetPxFromString(tmp->GetValue().GetArray()->GetValue(0)->GetString(), window_rect.x);
+      window_rect.y = dc->GetPxFromString(tmp->GetValue().GetArray()->GetValue(1)->GetString(), window_rect.y);
+    }
+    else
+      window_rect = window_rect.CenterIn(parent_rect);
+
+    // Make sure the window is inside the parent, and not larger.
+    window_rect = window_rect.MoveIn(parent_rect).Clip(parent_rect);
+
+    mMainWindow->SetRect(window_rect);
+
+    // mainWindow->SetOpacity(1);
+  }
+
+  mDemoSetup = true;
+  mHookEvents = true;
 }
 
 namespace Z
--- Code/Libraries/Extensions/TBUI/TBUIView.hpp
+++ Code/Libraries/Extensions/TBUI/TBUIView.hpp
@@ -8,5 +8,33 @@ class TBUIView : public tb::TBWidget
 public:
   /// Meta Initialization.
   ZilchDeclareType(TBUIView, TypeCopyMode::ReferenceType);
+
+  TBUIView();
+
+private:
+  void UpdateBatches();
+  void RenderBatches();
+  void RenderBatch(const TBUIBatch& batch);
+  void CreateRenderData(ViewBlock& viewBlock,
+                        FrameBlock& frameBlock,
+                        const Mat4& worldMatrix,
+                        const IntRect& clipRect,
+                        const Array<StreamedVertex>& vertices,
+                        const Texture* texture,
+                        PrimitiveType::Enum primitiveType);
+  ViewNode& AddRenderNodes(
+      ViewBlock& viewBlock, FrameBlock& frameBlock, const Mat4& worldMatrix, const IntRect& clipRect, const Texture* texture);
+
+private:
+  friend class TBUI;
+
+  Vec3 mTranslation;
+  float mAngle;
+  Mat4 mWorldTx = Mat4::cIdentity;
+
+  TBUI* mUI = nullptr;
+  TBUIRenderer* mRenderer = nullptr;
+
+  Array<TBUIBatch> mBatches;
 };
 } // namespace Zero
