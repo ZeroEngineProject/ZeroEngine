7aaba4285345e2b58c5d9cf82dafd31705c9f3ed
Robert Campbell <waprave@gmail.com>
2024-09-21 14:52:40 -0400

    Projects/ZeroLauncher Clean-up

--- Code/Libraries/Projects/ZeroLauncher/ActiveProjectMenu.cpp
+++ Code/Libraries/Projects/ZeroLauncher/ActiveProjectMenu.cpp
@@ -490,7 +490,7 @@ void ActiveProjectMenu::OnBackupProject(Event* e)
   String projectName = mCachedProject->GetProjectName();
 
   // Build the backup's file path based upon the project name and the current time date stamp.
-  String backupDirectory = FilePath::Combine(GetUserDocumentsDirectory(), "ZeroProjects", "Backups");
+  String backupDirectory = FilePath::Combine(GetUserDocumentsApplicationDirectory(), "Backups");
   String timeStamp = GetTimeAndDateStamp();
   String backupName = BuildString(projectName, timeStamp);
   String backupFilePath = FilePath::CombineWithExtension(backupDirectory, backupName, ".zip");
